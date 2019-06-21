4710e2eb3d76a56d0c72392a373723dccde58b88
Cristian Consonni <cristian@balist.es>
2019-06-21 23:44:02 +0200

    Add default values for optional arguments; fix indentation

--- utils/sgf-parse-log.py
+++ utils/sgf-parse-log.py
@@ -11,8 +11,8 @@ Argiments:
   <logfile>       Log file to read [default: stdin].
 
 Options:
-  --tz TIMEZONE               Timezone of the timestamps in the log file.
-  --time-format=TIME_FORMAT   Time format of the timestamps in the log file.
+  --tz TIMEZONE               Timezone of the timestamps in the log file [default: 'America/Toronto'].
+  --time-format=TIME_FORMAT   Time format of the timestamps in the log file  [default: 'YYYY MMM D HH:mm:ss'].
                               It can be specified multiple times.
   -h --help                   Show this screen.
   --version                   Show version.
@@ -29,14 +29,14 @@ def read_input(logfile):
         for line in lines:
           yield(line)
     else:
-      for infile in arguments['<logfile>']:
-        infp = open(infile, 'r')
-        for line in infp:
-          yield(line)
+        for infile in arguments['<logfile>']:
+            infp = open(infile, 'r')
+            for line in infp:
+                yield(line)
 
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='sgf-stats.py 0.0.1')
+    arguments = docopt(__doc__, version='sgf-stats.py 0.2')
 
     logfile = arguments['<logfile>']
     tz = arguments['--tz']
@@ -44,22 +44,22 @@ if __name__ == '__main__':
 
     for line in read_input(logfile):    
 
-      line = line.split()
+        line = line.split()
 
-      year = arrow.now().year
-      month = line[0]
-      day = line[1]
-      time = line[2]
+        year = arrow.now().year
+        month = line[0]
+        day = line[1]
+        time = line[2]
 
-      country = line[-1].strip('()')
-      ip_address = line[-2]
+        country = line[-1].strip('()')
+        ip_address = line[-2]
 
       
-      timestamp = arrow.get('{year} {month} {day} {time}'.format(year=year,
-                                                                 month=month,
-                                                                 day=day,
-                                                                 time=time),
-                            [tf for tf in time_formats]
-                            ).replace(tzinfo=dateutil.tz.gettz(tz))
+        timestamp = arrow.get('{year} {month} {day} {time}'.format(year=year,
+                                                                   month=month,
+                                                                   day=day,
+                                                                   time=time),
+                              [tf for tf in time_formats]
+                              ).replace(tzinfo=dateutil.tz.gettz(tz))
 
-      print(timestamp, country, ip_address)
+        print(timestamp, country, ip_address)
